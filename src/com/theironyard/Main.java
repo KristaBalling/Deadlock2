af627c5788e278a89c8917cf0fd462048a378731
Krista Blachian <krista.blachian@scifusion360.com>
2019-01-24 10:17:00 -0500

    Exercise complete

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -22,6 +22,13 @@ public class Main {
         }).start();
     }
 
+    //Thread1 acquires the lock on the jane object and enters the sayHello() method. It prints to the console, then suspends.
+    //Thread2 acquires the lock on the jonn object and enters the sayHello() mehtod. It prints to the console, then suspends.
+    //Thread1 runs again and wants to say hello back to the john object. It tries to call the sayHelloBack() method using the john
+    //object that was passed into the sayHello() method, but Thread2 is holding the john lock, so Thread1 suspends.
+    //Thread2 runs again and wants to say hello back to the jane object. It tries to call the sayHelloBack mehtod using the jane
+    // object that was passed into the sayHello() method, but Thread1 is holding the jane lock, so Thread2 suspends.
+
     static class PolitePerson {
         private final String name;
 
